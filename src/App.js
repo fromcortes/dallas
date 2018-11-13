24976a9651c4c3685160264df0eeb26da98c9eb5
Dennis Cortes <hi@cortes.us>
2018-11-12 18:21:06 -0600

    Starts adding info and classes for use

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="pink f1 ma5 b">
-          Dallas Boudreaux
-        </header>
+      <div className="App bg-white br2 ma5 pa5">
+        <div className="cf">
+          {/* Left Side */}
+          <div className="fl">
+            Left
+          </div>
+          {/* Right Side */}
+          <div className="fl">
+            <header className="lemonde">
+              <h1 className="f2 gold mb3">Dallas Boudreaux</h1>
+              <h2 className="black-60 f3 fw4 i mt3">Actor, Singer, Voice Actor</h2>
+            </header>
+          </div>
+        </div>
       </div>
     );
   }
